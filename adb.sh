c2a9195f093f452ab14dc054ccf5495432bb8fb7
gavinshao111 <yiyecheer@163.com>
2017-11-02 20:29:16 +0800

    update

--- adb.sh
+++ adb.sh
@@ -15,3 +15,9 @@ adb install *.apk
 
 # 复制手机文件到电脑
 adb pull /data/app/com.supercell.clashroyale.wdj-1/base.apk D:\Android\apk
+
+# 卸载app + 包名
+adb uninstall com.hase.bclm.client  
+
+# 捕获日志
+adb logcat -s ${TAG_NAME}
